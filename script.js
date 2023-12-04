51ad4ab9a89b67315123c9fff3c109258e440072
Coco <143611879+cfrikke@users.noreply.github.com>
2023-12-04 14:24:40 +0000

    e

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-document.addEventListener( 'scroll', function () {
-    var pre = document.querySelector( 'html' );
-    pre.innerHTML = pre.innerHTML + "<div id='redbox'></div>\n";
-    pre.innerHTML = pre.innerHTML + "<div id='redorangebox'></div>\n";
-    pre.innerHTML = pre.innerHTML + "<div id='yellowbox'></div>\n";
-    pre.innerHTML = pre.innerHTML + "<div id='yellowgreenbox'></div>\n";
-    pre.innerHTML = pre.innerHTML + "<div id='greenbox'></div>\n";
-    pre.innerHTML = pre.innerHTML + "<div id='tealbox'></div>\n";
-    pre.innerHTML = pre.innerHTML + "<div id='bluebox'></div>\n";
-    pre.innerHTML = pre.innerHTML + "<div id='dsbluebox'></div>\n";
-    pre.innerHTML = pre.innerHTML + "<div id='indigobox'></div>\n";
-    pre.innerHTML = pre.innerHTML + "<div id='bvbox'></div>\n";
-    pre.innerHTML = pre.innerHTML + "<div id='violetbox'></div>\n";
+document.addEventListener('scroll', function() {
+  const node = document.getElementById("box");
+  const clone = node.cloneNode(true);
+    document.getElementById("body").appendChild(clone);
   });
+
+
