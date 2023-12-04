578fcf6015cc95c0faac0438791a9d36aaa31ee3
Coco <143611879+cfrikke@users.noreply.github.com>
2023-12-04 14:25:32 +0000

    we

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-document.addEventListener('scroll', function() {
   const node = document.getElementById("box");
   const clone = node.cloneNode(true);
+document.addEventListener('scroll', function() {
+  node = document.getElementById("box");
+  clone = node.cloneNode(true);
     document.getElementById("body").appendChild(clone);
   });
 
